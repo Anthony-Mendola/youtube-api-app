1660bc79d392ba48cc56ee98418fe59da2ffb950
Anthony <github email address>
2018-05-18 14:15:24 -0400

    adding jquery to loop through playlist videos

--- custom.js
+++ custom.js
@@ -16,13 +16,29 @@ $(document).ready(function() {
   function loadVids() {
     $.getJSON(URL, options, function(data){
       console.log(data)
-      mainVid();
+      var id = data.items[0].snippet.resourceId.videoId;
+      mainVid(id);
+      resultsLoop();
     })
   }
 
-  function mainVid() {
-    $('#video').html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/3AkjzDHuWKM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+  function mainVid(id) {
+    $('#video').html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     `);
+  }
+
+  function resultsLoop() {
+
+    $('main').html(`<article>
+      <img src="https://i.imgur.com/BhRFjf4.jpg" alt="" class="thumb">
+
+      <div class="details">
+        <h4>Title</h4>
+        <p>Description</p>
+      </div>
+    </article>
+      `)
+
   }
 
 });
